be78ae1def4e43c505527404f7f4e846f79ce0be
B-e-sa <joaospereira963@gmail.com>
2024-11-22 09:28:52 -0300

    format code

--- util.py
+++ util.py
@@ -1,18 +1,19 @@
 import easyocr
 import re
 
-reader=easyocr.Reader(['en'],gpu=False)
+reader = easyocr.Reader(['en'], gpu=False)
+
 
 def read_license_plate(license_plate_crop):
-    detections=reader.readtext(license_plate_crop)
+    detections = reader.readtext(license_plate_crop)
 
-    full_text=''
+    full_text = ''
     for detection in detections:
-        _,text,_=detection
-        full_text+=text.upper().replace(' ', '')     
-    
+        _, text, _ = detection
+        full_text += text.upper().replace(' ', '')
+
     # brazilian plate regex
     # like RIO1A18
-    plate_match=re.search(r'[A-Z]{3}[0-9][A-Z0-9][0-9]{2}',full_text)
-    
-    return full_text,(plate_match.group(0))if(plate_match is not None)else(None)
+    plate_match = re.search(r'[A-Z]{3}[0-9][A-Z0-9][0-9]{2}', full_text)
+
+    return full_text, (plate_match.group(0))if (plate_match is not None)else (None)
