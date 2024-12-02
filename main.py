4e093298a71d6baee62f7599b003f52659e46039
b_e_sa <joaospereira963@gmail.com>
2024-12-01 22:09:03 -0300

    add error handling and detection visual clues

--- main.py
+++ main.py
@@ -1,47 +1,88 @@
 from ultralytics import YOLO
 import cv2
 import serial
+import time
 from util import read_license_plate
 
-PLACAS_CADASTRADAS = [""]
+REGISTERED_PLATES = ["RIO9A27"]
 LICENSE_PLATE_DETECTOR = YOLO('license_plate_detector.pt')
-ARDUINO_PORT = serial.Serial("COM3", 9600)
+
+# try catch que garante que a porta do arduíno
+# está ativa e conectada à aplicação
+try:
+    # porta serial que se comunicará ao arduino
+    # os parâmetros são, respectivamente,
+    # a porta do arduíno que será conectada
+    # e a velocidade de transmissão dos dados (baudrate)
+    ARDUINO_PORT = serial.Serial("COM3", 9600, timeout=1)
+
+    time.sleep(2)  # aguardar estabilização da conexão
+    print("Conexão estabelecida.")
+
+except serial.SerialException as e:
+    print(f"Erro na conexão serial: {e}")
+    exit(1)
+
+# cores RGB utilizadas
+RED = (255, 0, 0)
+GREEN = (0, 0, 255)
+
+# após deteção da placa, código não enviará sinais
+# para o arduino, de forma a evitar sobrecargas
+INTERVAL_POST_DETECTION = 5
+in_interval = False
+last_detection_time = time.time()
+
 CAP = cv2.VideoCapture(0)
 ret = True
-previous_license_plate = ""
 while ret:
     ret, frame = CAP.read()
     if ret:
         LICENSE_PLATES = LICENSE_PLATE_DETECTOR(frame, verbose=False)[0]
         for license_plate in LICENSE_PLATES.boxes.data.tolist():
             x1, y1, x2, y2, score, class_id = license_plate
+
+            # evitando erros de valores float ao cortar
+            # a região da placa na imagem
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
             LICENSE_PLATE_CROP = frame[y1:y2, x1:x2, :]
             LICENSE_PLATE_CROP_GRAY = cv2.cvtColor(
                 LICENSE_PLATE_CROP, cv2.COLOR_BGR2GRAY)
 
-            ambiguous_license_plate_text, correct_license_plate_text = read_license_plate(
+            text = None
+
+            formated_plate, unformatted_plate = read_license_plate(
                 LICENSE_PLATE_CROP_GRAY)
 
-            text = None
-            if correct_license_plate_text:
-                previous_license_plate = correct_license_plate_text
-                text = correct_license_plate_text
-            if len(ambiguous_license_plate_text) == 7:
-                previous_license_plate = ambiguous_license_plate_text
-                text = ambiguous_license_plate_text
+            if formated_plate:
+                previous_license_plate = formated_plate
+                text = formated_plate
+
+            elif len(unformatted_plate) == 7:
+                previous_license_plate = unformatted_plate
+                text = unformatted_plate
+
             if text is None:
                 text = previous_license_plate
+                continue
 
-            if text in PLACAS_CADASTRADAS:
-                ARDUINO_PORT.write(b'1')
-            else:
-                ARDUINO_PORT.write(b'0')
+            PLACA_CADASTRADA = text in REGISTERED_PLATES
 
-            cv2.putText(frame, text, (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            # enviando sinal positivo para leitura do arduíno
+            if PLACA_CADASTRADA:
+                if not in_interval:
+                    ARDUINO_PORT.write(b'1')
+                    in_interval = True
+                    last_detection_time = time.time()
+
+            COR_DETECCAO = GREEN if PLACA_CADASTRADA else RED
+
+            cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, COR_DETECCAO, 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), COR_DETECCAO, 1)
+
+        if in_interval and (time.time() - last_detection_time) > INTERVAL_POST_DETECTION:
+            in_interval = False
 
         cv2.imshow('Câmera', frame)
         if (cv2.waitKey(1) & (0xFF == ord('q'))):
--- util.py
+++ util.py
@@ -3,17 +3,29 @@ import re
 
 reader = easyocr.Reader(['en'], gpu=False)
 
-
 def read_license_plate(license_plate_crop):
+    """
+    Lê o texto de uma imagem de placa veicular e tenta identificar uma placa no formato brasileiro.
+
+    Args:
+        license_plate_crop (str ou ndarray): A imagem contendo a placa veicular.
+
+    Returns:
+        tuple: 
+            - plate (str): A placa detectada na imagem, se ela estiver formatada no padrão Mercosul.
+            - unformatted_plate (str ou None): A placa detectada se ela tiver 7 carácteres, mas não estiver no padrão Mercosul.
+    """
     detections = reader.readtext(license_plate_crop)
 
-    full_text = ''
+    # as deteccões de frases/caracteres sao feitas em partes,
+    # então elas são unidos no loop
+    plate = ''
     for detection in detections:
         _, text, _ = detection
-        full_text += text.upper().replace(' ', '')
+        plate += text.upper().replace(' ', '')
 
-    # brazilian plate regex
-    # like RIO1A18
-    plate_match = re.search(r'[A-Z]{3}[0-9][A-Z0-9][0-9]{2}', full_text)
+    # vê se a placa tem formatação brasileira mercosul,
+    # como: BRA1C23 
+    plate_match = re.search(r'[A-Z]{3}[0-9][A-Z0-9][0-9]{2}', plate)
 
-    return full_text, (plate_match.group(0))if (plate_match is not None)else (None)
+    return (plate_match.group(0)) if (plate_match is not None) else None, plate
